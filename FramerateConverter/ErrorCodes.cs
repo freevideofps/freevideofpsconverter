9f118b387fe011d69cb4a8c94a514fc75db2c58a
freevideofps <51317899+freevideofps@users.noreply.github.com>
2014-08-06 09:52:23 +0000

    Initial commit

---
+++ FramerateConverter/ErrorCodes.cs
@@ -0,0 +1,26 @@
+﻿//0 	OK
+//1 	target exists/overwrite impossible, not requested
+//2 	user canceled
+//3 	no frame rate change
+//4 	illegal frame rate
+//5     illegal command on command line detected
+//6     source file corrupt
+//7     initial license was not accepted
+//100 	unknown error
+
+namespace FramerateConverter
+{
+    public enum ErrorCodes
+    {
+        Ok = 0,
+        TargetExists = 1,
+        Canceled = 2,
+        NoFpsChange = 3,
+        IllegalFps = 4,
+        IllegalCommand = 5,
+        SourceFileNoAcceptable = 6,
+        LicenseNotAccepted = 7,
+        TargetFileCorrupt = 8,
+        Unknown = 100,
+    }
+}
---
+++ FramerateConverter/Options.cs
@@ -0,0 +1,36 @@
+﻿using CommandLine;
+
+//command line parameters:
+//====================
+//-b, --batch					no
+//-s, --source					--
+//-t, --target					--
+//-k, --key-frames-only			no
+//-f, --frames-per-second		input fps * 2
+//-o, --overwrite				ask
+//-v, --version					--
+//-h, --help					--
+
+namespace FramerateConverter
+{
+    public class Options
+    {
+        [Option('b', "batch", Required = false, HelpText = "Enable batch behavior: Run once, then terminate automatically.")]
+        public bool BatchMode { get; set; }
+
+        [Option('s', "source", Required = false, HelpText = "The input video.")]
+        public string SourceFile { get; set; }
+
+        [Option('t', "target", Required = false, HelpText = "The output video.")]
+        public string TargetFile { get; set; }
+
+        [Option('k', "key-frames-only", Required = false, HelpText = "Enables Key-Frame-Only videos.")]
+        public bool KeyFramesOnly { get; set; }
+
+        [Option('f', "frames-per-second", Required = false, HelpText = "The target FPS.")]
+        public int? FramesPerSecond { get; set; }
+
+        [Option('o', "overwrite", Required = false, HelpText = "If output video exists, it will be overwritten.")]
+        public bool Overwrite { get; set; }
+    }
+}
